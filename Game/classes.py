962cad9addaf064cd00f2c516f5ab334c9a6cd04
AhouLucas <ahou.lucas@gmail.com>
2023-03-04 20:17:44 +0100

    add music to the game

--- Game/classes.py
+++ Game/classes.py
@@ -1,12 +1,61 @@
 import pygame as pg
 
+
+class Gauge:
+    def __init__(self, position, size, color, max_value):
+        self.position = position
+        self.size = size
+        self.color = color
+        self.max_value = max_value
+        self.value = 0
+
+    def show(self, screen):
+        pg.draw.rect(screen, self.color, (self.position, (self.size[0], self.size[1] * self.value / self.max_value)))
+
+    def add(self, value):
+        self.value += value
+        if self.value > self.max_value:
+            self.value = self.max_value
+
+    def update(self, value):
+        self.value = value
+
+    def is_empty(self):
+        return self.value <= 0
+
 class Player:
     def __init__(self, position, pseudo, image=None, victory_scream=None):
         self.pseudo = pseudo
         self.image = image
-        self.drunkness_level = 0
         self.dead = False
         self.rect = pg.Rect(position, (50, 100))
+        self.drunkness_level = Gauge((0, 0), (50, 100), (0, 255, 0), 100)
+        
 
-    def show(self, surface):
-        pg.draw.rect(surface, (255, 0,0), self.rect)
+    def show(self, screen):
+        pg.draw.rect(screen, (255, 0,0), self.rect)
+        self.drunkness_level.show(screen)
+
+    def drink(self, drink):
+        self.drunkness_level.add(drink.type.alcohol)
+
+
+class DrinkType:
+    def __init__(self, image, alcohol):
+        self.image = image
+        self.alcohol = alcohol
+
+class Drink:
+    TYPE = (
+        DrinkType(r"images/drink0.png", 10),
+        DrinkType(r"images/drink1.png", 20),
+        DrinkType(r"images/drink2.png", 30)
+    )
+    
+    def __init__(self, type, position):
+        self.type = Drink.TYPE[type]
+        self.position = position
+
+    def show(self, screen):
+        img = pg.image.load(self.type.image)
+        screen.blit(img, self.position)
--- Game/main.py
+++ Game/main.py
@@ -5,15 +5,23 @@ from classes import Player
 
 pg.init()
 pg.display.set_caption("Hackathon : Edition 24H !")
+clock = pg.time.Clock()
 ASPECT_RATIO = 16/9
 WIDTH = 1000; HEIGHT = WIDTH/ASPECT_RATIO
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 
 ###############################
 
+
+#### Variables ####
+running = True
+# Set this to True to start the game
+start = False
+
 Player_1, Player_2 = Player((WIDTH/3, HEIGHT/3), "Player 1"), Player((2*WIDTH/3, HEIGHT/3), "Player 2")
 Player1_drinks = []
 Player2_drinks = []
+###################
 
 #### Functions ####
 def spawn_drink(player: int):
@@ -27,17 +35,53 @@ def spawn_drink(player: int):
     elif player == 2:
         Player2_drinks.append(Drink())
 
+    for drink in Player1_drinks:
+        drink.show(screen)
+        drink.animate()
+    for drink in Player2_drinks:
+        drink.show(screen)
+        drink.animate()
+
+
+def play_music(filename):
+    pg.mixer.pre_init(44100, -16, 2, 2048) # setup mixer to avoid sound lag
+    pg.init()
+    pg.mixer.init()
+    pg.mixer.music.load(filename)
+    pg.mixer.music.play(loops=-1)
+
+def start_menu():
+    """Displays the start menu.
+    """
+    global start
+    screen.fill((255, 0, 0))
+    clock.tick(60)
+    pg.display.flip()
 
 def main():
-    while True:
+    global running, start
+    play_music('Musics/Start Menu.mp3')
+    while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 return
-        screen.fill((255, 255, 255))
-        Player_1.show(screen)
-        Player_2.show(screen)
-        pg.display.flip()
+            elif start == False and event.type == pg.MOUSEBUTTONDOWN:
+                start = True
+                pg.mixer.music.stop()
+                play_music('Musics/Gameplay.mp3')
+
+        
+        # Game loop only if start is True
+        # Start menu
+        if not start:
+            start_menu()
+        else:
+            screen.fill((255, 255, 255))
+            Player_1.show(screen)
+            Player_2.show(screen)
+            pg.display.flip()
+            clock.tick(60)
             
 
 if __name__ == "__main__":

3d03f17500be0bcd8fc6b7fc5fda200a3de70b8c
AhouLucas <ahou.lucas@gmail.com>
2023-03-05 13:16:38 +0100

    add microphone and time measure

--- Game/network.py
+++ Game/network.py
@@ -4,9 +4,11 @@ import json
 
 def socket_read(client, data_list):
     while True:
-        data = client.recv(1024).decode()  # receive response
-        data_list.append(json.loads(data))
-
+        try:
+            data = client.recv(1024).decode()  # receive response
+            data_list.append(json.loads(data))
+        except:
+            pass
     client.close()  # close the connection
 
 if __name__ == '__main__':

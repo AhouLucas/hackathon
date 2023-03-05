5d2a8ab944dbed148977ac82439b8e579f31f271
adityachugh02 <60618118+adityachugh02@users.noreply.github.com>
2023-03-05 12:25:42 +0100

    Add files via upload

--- Game/classes.py
+++ Game/classes.py
@@ -24,40 +24,28 @@ class Gauge:
         return self.value <= 0
 
 class Player:
-    vomitingKey = 80
-    initKey = 60
+    vomitingKey = 90
+    initKey = 70
     normalKey = 40
     armKey = 30
     drinkingKey = 20
 
-    hOffset = 0
-    vOffset = 0
-
-    def __init__(self, position,size, pseudo, image=None, victory_scream=None):
+    def __init__(self, position, size, pseudo, image=None, victory_scream=None):
+        self.position = position
         self.pseudo = pseudo
         self.image = image
         self.dead = False
         self.width, self.height = size
         self.rect = pg.Rect(position, (self.width, self.height))
-        self.rect.center = position
-        self.position = self.rect.center
         self.drunkness_level = Gauge((position[0]-200, position[1]-50), (50, 200), (0, 255, 0), 1000)
         self.animationStatus = Player.initKey
         self.drinkInHand = None
-        self.lastAddedDrink = 0
-        self.countdown = 20
         
         self.images = {
             "vomit": pg.transform.scale(pg.image.load("Images/sprites/vomit.png"), (self.width, self.height)),
             "normal": pg.transform.scale(pg.image.load("Images/sprites/happy.png"), (self.width, self.height)),
-            "arm": pg.transform.scale(pg.image.load("Images/sprites/arm.png"), (self.width, self.height)),
-            "drinking": pg.transform.scale(pg.image.load("Images/sprites/drinking.png"), (self.width, self.height)),
-            "bucket": pg.transform.scale(pg.image.load("Images/sprites/bucket.png"), (self.width, self.height))
-        }
-
-        self.flags = {
-            "flip": False,
-            "drunk": False
+            "arm": pg.transform.scale(pg.image.load("Images/sprites/arm-with-glass.png"), (self.width, self.height)),
+            "drinking": pg.transform.scale(pg.image.load("Images/sprites/drinking-with-glass.png"), (self.width, self.height)),
         }
 
     def show(self, screen):
@@ -65,8 +53,6 @@ class Player:
             img = self.images["vomit"]
             screen.blit(img, (self.position[0], self.position[1]))
             
-            img = self.images["bucket"]
-            screen.blit(img, (self.position[0] + Player.hOffset, self.position[1] + 150))
             self.animationStatus -= 1
         
         if Player.normalKey <= self.animationStatus <= Player.initKey:
@@ -86,41 +72,24 @@ class Player:
             screen.blit(img, (self.position[0], self.position[1]))
             
             self.animationStatus -= 1
-        
+            
         if self.animationStatus < Player.armKey:
             img = self.images["drinking"]
             screen.blit(img, (self.position[0], self.position[1]))
 
-            if not self.flags["flip"]: 
-                self.drinkInHand.img = pg.transform.flip(self.drinkInHand.img, False, True)
-                self.flags["flip"] = True
-            if not self.flags["drunk"]: 
-                self.drinkInHand.position[0] -= 95
-                self.drinkInHand.position[1] -= 425
-                self.flags["drunk"] = True 
+            #self.drinkInHand.position = (300, 300)
             self.drinkInHand.show(screen)
-            if self.flags["drunk"]:
-                self.drinkInHand.position[0] += 95
-                self.drinkInHand.position[1] += 425
-                self.flags["drunk"] = False
+            # self.drinkInHand.position[1] += 100
             
             self.animationStatus -= 1
-           
-        if self.animationStatus == Player.drinkingKey:
-            self.drunkness_level.add(self.drinkInHand.type.alcohol)
-            # if self.drunkness_level.value >= self.drunkness_level.max_value:
-            #     img = self.images["drunk"]
-            #     screen.blit(img, (self.position[0] + Player.hOffset, self.position[1] + Player.vOffset))
-                # self.dead = True
             
+        if self.animationStatus == Player.drinkingKey:
             self.animationStatus = Player.initKey
-            self.flags["flip"] = False
-            self.countdown -= 1
-    
+        
         self.drunkness_level.show(screen)
 
     def drink(self, drink):
-        # self.drunkness_level.add(drink.type.alcohol)
+        self.drunkness_level.add(drink.type.alcohol)
         self.animationStatus = Player.initKey - 1
         # self.drinkInHand = Drink(Drink.TYPE.index(drink.type), drink.position)
         self.drinkInHand = drink
@@ -136,10 +105,9 @@ class DrinkType:
 
 class Drink:
     TYPE = (
-        DrinkType(r"Images/sprites/small glass.png", 30),
-        DrinkType(r"Images/sprites/big glass.png", 50),
-        DrinkType(r"Images/sprites/bottle.png", 70),
-        DrinkType(r"Images/sprites/gordon.png", 90)
+        DrinkType(r"Images/sprites/small glass.png", 10),
+        DrinkType(r"Images/sprites/bottle.png", 20),
+        DrinkType(r"Images/sprites/gordon.png", 30)
     )
     
     def __init__(self, type, position):
@@ -153,7 +121,8 @@ class Drink:
         #screen.blit(self.img, (self.position[0], self.position[1]))
         
     def animate(self, screen):
-        if self.position[1] > 3 * screen.get_size()[1] // 5:
-            self.position[1] -= 25
-            self.show(screen)
+        pass
+        #if self.position[1] > 3 * screen.get_size()[1] // 5:
+        #    self.position[1] -= 15
+        #self.show(screen)
  
